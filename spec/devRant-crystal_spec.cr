5d76839cc6a566abb0c0591dd57359598feabbc6
iostreamer-X <iostreamer007@gmail.com>
2016-08-14 19:44:46 +0530

    async test added

--- spec/devRant-crystal_spec.cr
+++ spec/devRant-crystal_spec.cr
@@ -3,6 +3,16 @@ require "json"
 
 describe DevRant do
 
+  it "getIdByUsernameAsync" do
+    id = nil
+    DevRant.getIdByUsernameAsync "coookie",->(user_id : String){
+      puts user_id
+      id = user_id
+    }
+    sleep 2.seconds
+    id.should eq "82833"
+  end
+
   it "getIdByUsername" do
     DevRant.getIdByUsername("coookie").should eq "82833"
   end
